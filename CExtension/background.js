8d667007eb4d071593fbe04ce5c0e18339ae4290
nottec <160436048+nottec@users.noreply.github.com>
2025-06-06 00:32:32 -0500

    Add files via upload
    Added a service worker so that clicks are passively gained even when away

---
+++ CExtension/background.js
@@ -0,0 +1,33 @@
+console.log("background is here")
+let startTime;
+chrome.storage.local.get(['passivecount'], (data) => {
+if(data.passivecount){
+    console.log("its the port connection")
+}
+});
+
+chrome.runtime.onConnect.addListener((port) => {
+    if (port.name==="popup") {
+        console.log("bgside popupwindow open connect")
+        chrome.storage.local.get(['startTime', 'passivecount','count','vgcount'], (data) => {
+            if(data.passivecount){
+                console.log("passive count is true")
+                const elapsedTime = Date.now() - (data.startTime??Date.now());
+                const seconds = Math.floor((elapsedTime) / 1000);
+                const newcount = (data.count ?? 0) + ((data.vgcount ?? 0)*seconds);
+                console.log ((data.count??0) + "-->"+(newcount))
+                chrome.storage.local.set({newcount})
+                port.postMessage("data_ready")
+            }
+        });
+
+        port.onDisconnect.addListener(() => {
+            console.log("bgside popup disconnect; closed")
+            chrome.storage.local.set({
+                startTime: Date.now()
+            }); 
+        });
+    }
+})
+
+
